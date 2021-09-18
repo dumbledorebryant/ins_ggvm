7145e9b6727d30f345e3c12e4320a9f7e4a2c4be
dumbledorebryant <dumbledorebryant@gmail.com>
2021-09-17 20:48:22 -0400

    add comment

--- echo_server.py
+++ echo_server.py
@@ -14,25 +14,28 @@ import socket
 def main():
     parser = argparse.ArgumentParser()
 
-    # add a required argument, which can either be specified
-    # via -n or --name.  Store the result in a variable called "name"
-    parser.add_argument('-p', '--port', dest = 'port', 
-                                        help = 'connects to this port', 
-                                        type = int, 
-                                        required=True)
+    # via -p. Store the result in a variable called "port"
+    parser.add_argument('-p', dest = 'port', 
+                              help = 'connects to this port', 
+                              type = int, 
+                              required=True)
 
     # add a command-line argument that has no parameter -- i.e., it's true iff it's specified
     # note that "store_true" is a special string that tells argparse what to store if this
-    # command-line argument is provided
-    parser.add_argument('-r', '--reverse', dest='reverse', 
-                                           help='reverse the input', 
-                                           action="store_true")
+    parser.add_argument('-r', dest='reverse', 
+                              help='reverse the input', 
+                              action="store_true")
     
+    # get all the args
     args = parser.parse_args()
     
+    # get port number
     port = args.port
+
+    # get reverse flag
     reverse = args.reverse
 
+    # make the server waiting for a upcoming client
     host = "127.0.0.1"
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     address = (host, port)
@@ -40,24 +43,41 @@ def main():
     server.listen(5)
     
     while(True):
+
+        # waiting start
         print("waiting")
         conn, addr = server.accept()
+
         while(True): 
+            # receving start
             print("receving")
             data = conn.recv(1024)
+
+            # check if the connection is broken
             if data == b'': 
                 break
+
+            # get the data from client
             output = data.decode('utf-8')
+
+            # check if the output should be reversed or not
             if reverse is True:
+
+                # reverse it
                 output = output[::-1]
+
                 bytesss = bytes(output, encoding='utf-8')
                 bytesss = bytesss + '\n'.encode('utf-8')
                 newbytes = bytesss[1:]
                 conn.send(newbytes)
+            
             else:
+                # just send the original msg back
                 conn.send(data)
+
             print("one message done")
     
+    # shut down the server
     server.close()
     print("end")
 
